d8f7f444f8f5f0eb55715a915b3e1b2fe6253e81
Tahseen <tahseenislam@hotmail.com>
2022-02-11 18:18:25 +1000

    fixed main.js and favico error for index.html and contact.html

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-//API------ https://api.pray.zone/v2/times/this_month.json?city=wakefield
-
-// target prayer__list class
-
+//Get todays day in the current month
 function getTodayDate() {
-  var TodayDate = new Date();
-  var DayInCurrMonth = TodayDate.getDate();
+  const TodayDate = new Date();
+  const DayInCurrMonth = TodayDate.getDate();
   return DayInCurrMonth;
 }
 
-console.log(getTodayDate());
+// console.log(getTodayDate());
 
 async function main() {
   const times = await fetch(
@@ -16,13 +13,13 @@ async function main() {
   );
 
   const { results } = await times.json();
-  const currentDayPrayerTimes = results.datetime[getTodayDate()];
+  // console.log(results);
+  const currentDayPrayerTimes = results.datetime[getTodayDate()].times;
 
   // console.log(results.datetime);
-  console.log(currentDayPrayerTimes);
+  // console.log(currentDayPrayerTimes);
 
   const prayerListEl = document.body.querySelector('.prayer__list');
-
   prayerListEl.innerHTML = prayerHTML(currentDayPrayerTimes);
 }
 
@@ -31,22 +28,22 @@ main();
 function prayerHTML(prayer) {
   return `<div class="prayer">
       <h2 class="prayer__title">Fajr</h2>
-      <h2 class="prayer__time">${prayer.times.Fajr}</h2>
+      <h2 class="prayer__time">${prayer.Fajr}</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Zuhr</h2>
-    <h2 class="prayer__time">${prayer.times.Dhuhr}</h2>
+    <h2 class="prayer__time">${prayer.Dhuhr}</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Asr</h2>
-    <h2 class="prayer__time">${prayer.times.Asr}</h2>
+    <h2 class="prayer__time">${prayer.Asr}</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Magrib</h2>
-    <h2 class="prayer__time">${prayer.times.Maghrib}</h2>
+    <h2 class="prayer__time">${prayer.Maghrib}</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Isha</h2>
-    <h2 class="prayer__time">${prayer.times.Isha}</h2>
+    <h2 class="prayer__time">${prayer.Isha}</h2>
     </div>`;
 }
