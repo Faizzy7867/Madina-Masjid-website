12b4226f6d47626fe116475e7336cbb3ab39462e
Tahseen <tahseenislam@hotmail.com>
2022-02-20 00:11:53 +1000

    added Jummah Time

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const todayDate = new Date();
 const month = todayDate.getMonth() + 1;
 
 async function main() {
-  const promise = await fetch("./assets/JamaatTime.json");
+  const promise = await fetch('./assets/JamaatTime.json');
   const data = await promise.json();
 
   const todayDate = new Date();
@@ -11,48 +11,56 @@ async function main() {
   const currMonth = data[month][day];
   // console.log(currMonth);
 
-  const Elprayer = document.body.querySelector(".prayer__list");
+  const Elprayer = document.body.querySelector('.prayer__list');
   Elprayer.innerHTML = prayerHTML(currMonth);
 }
 
 main();
 
 function prayerHTML(prayer) {
+
+  let timeAMPM = prayer.Zhur.slice(0, 2);
+
+
   return `<div class="prayer">
       <h2 class="prayer__title">Fajr</h2>
-      <h2 class="prayer__time">${prayer.Fajar}</h2>
+      <h2 class="prayer__time">${prayer.Fajar}am</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Zuhr</h2>
-    <h2 class="prayer__time">${prayer.Zhur}</h2>
+    <h2 class="prayer__time">${timeAMPM === '11' ? `${prayer.Zhur} am` : `${prayer.Zhur} pm`}</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Asr</h2>
-    <h2 class="prayer__time">${prayer.Asar}</h2>
+    <h2 class="prayer__time">${prayer.Asar}pm</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Magrib</h2>
-    <h2 class="prayer__time">${prayer.Maghrib}</h2>
+    <h2 class="prayer__time">${prayer.Maghrib}pm</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Isha</h2>
-    <h2 class="prayer__time">${prayer.Isha}</h2>
+    <h2 class="prayer__time">${prayer.Isha}pm</h2>
+    </div>
+    <div class="prayer">
+    <h2 class="prayer__title">Jummah</h2>
+    <h2 class="prayer__time">1:00pm</h2>
     </div>`;
 }
 
 function setToCurrentMonth() {
-  localStorage.setItem("month", month);
+  localStorage.setItem('month', month);
 }
 
 // BURGER MENU
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
 });
 
 // Set current year
-const yearEl = document.querySelector(".year");
+const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
