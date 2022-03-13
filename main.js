61a69f8336b1016197a86a1803299238f9dd6d61
Faizzy7867 <faizanfarooq000@gmail.com>
2022-03-13 13:46:54 +0000

    changed the sticky navigation style

--- main.js
+++ main.js
@@ -22,29 +22,29 @@ function prayerHTML(prayer) {
 
   return `<div class="prayer">
       <h2 class="prayer__title">Fajr</h2>
-      <h2 class="prayer__time">${prayer.Fajar}am</h2>
+      <h2 class="prayer__time">${prayer.Fajar} am</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Zuhr</h2>
     <h2 class="prayer__time">${
-      timeAMPM === "11" ? `${prayer.Zhur}am` : `${prayer.Zhur}pm`
+      timeAMPM === "11" ? `${prayer.Zhur} am` : `${prayer.Zhur} pm`
     }</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Asr</h2>
-    <h2 class="prayer__time">${prayer.Asar}pm</h2>
+    <h2 class="prayer__time">${prayer.Asar} pm</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Magrib</h2>
-    <h2 class="prayer__time">${prayer.Maghrib}pm</h2>
+    <h2 class="prayer__time">${prayer.Maghrib} pm</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Isha</h2>
-    <h2 class="prayer__time">${prayer.Isha}pm</h2>
+    <h2 class="prayer__time">${prayer.Isha} pm</h2>
     </div>
     <div class="prayer">
     <h2 class="prayer__title">Jummah</h2>
-    <h2 class="prayer__time">1:00pm</h2>
+    <h2 class="prayer__time">1:00 pm</h2>
     </div>`;
 }
 
@@ -61,29 +61,29 @@ btnNavEl.addEventListener("click", function () {
 });
 
 // Sticky Navigation
-const sectionHeroEl = document.querySelector(".section-hero");
+// const sectionHeroEl = document.querySelector(".section-hero");
 
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
 
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
 
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionHeroEl);
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     // In the viewport
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(sectionHeroEl);
 
 // Set current year
 const yearEl = document.querySelector(".year");
